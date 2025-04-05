b21b6782f5fb6568c67f99adae2ae3bfb2b8f0bb
JJJJJJpl <jedrek.stf@interia.pl>
2025-04-05 22:58:27 +0200

    bruteforce working1

--- modules/bruteforce_threaded.py
+++ modules/bruteforce_threaded.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from itertools import permutations
 from PIL import Image
 import numpy as np
 from collections import Counter
@@ -6,27 +7,34 @@ import string
 from concurrent.futures import ThreadPoolExecutor
 import threading
 
-def run(filename, treshold, max_workers=4):
+def run(filename, window_size, treshold, max_workers=4):
     image = Image.open(filename)
-    image = image.convert("RGB")
+    if image.mode != 'RGBA':
+      image = image.convert("RGB")
     lock = threading.Lock()
     
-    def process_e(e):
-        if e is not None:
-            score1 = character_distribution_score(e)
-            score2 = doubles_score(e)
-            if score1 > treshold and score2 > treshold:
-                with lock:
-                    print("Potential text detected:", e, "scores:", score1, score2)
-    
-    with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        for a in BlockIter1(image):
-            for b in OrderIter2(a):
-                for c in ColorIter3(b):
-                    for d in BitsIter4(c):
-                        for e in DecodingIter5(d):
-                            executor.submit(process_e, e)
-    
+    with open("output.txt", 'w') as output_file:
+      def process_e(e):
+         if len(e) > 0:
+            f = check(e, window_size, treshold, entropy_score)
+            if len(f) > 0:
+               g = check(e, window_size, treshold, character_distribution_score)
+               if len(g) > 0:
+                  h = check(e, window_size, treshold, doubles_score)
+                  if len(h) > 0:
+                     with lock:
+                        output_file.write(h)
+                        output_file.write("\n\n")
+                        #print("Potential text detected:", h)
+      
+      with ThreadPoolExecutor(max_workers=max_workers) as executor:
+         for a in BlockIter1(image):
+               for b in OrderIter2(a):
+                  for c in ColorIter3(b):
+                     for d in BitsIter4(c):
+                           for e in DecodingIter5(d):
+                              executor.submit(process_e, e)
+      
     print("Bruteforce finished")
 
 class BlockIter1:
@@ -51,6 +59,7 @@ class BlockIter1:
             case 1:
                 return self.img
             case 2:
+                raise StopIteration
                 return self.img[:self.hor//2,:self.wer//2]
             case 3:
                 return self.img[self.hor//2:,:self.wer//2]
@@ -84,33 +93,38 @@ class OrderIter2:
          raise StopIteration
          
 class ColorIter3:
-   def __init__(self,array):
-      self.arr = array
-      self.i = 0
-   def __iter__(self):
-      return self
-   def __next__(self):
-      self.i += 1
-      if self.arr.shape[1] == 3:
-        match self.i:
-            case 1:
-                return self.arr.reshape(-1)
-            case 2:
-                return np.concatenate([self.arr[:,0], self.arr[:,1],self.arr[:,2]])
-            case 3:
-                raise StopIteration
-      else:
-        match self.i:
-            case 1:
-                return self.arr.reshape(-1)
-            case 2:
-                return self.arr[:, :3].reshape(-1)
-            case 3:
-                return np.concatenate([self.arr[:,0], self.arr[:,1],self.arr[:,2]])
-            case 4:
-                return np.concatenate([self.arr[:,0], self.arr[:,1],self.arr[:,2],self.arr[:,3]])
-            case 5:
-              raise StopIteration
+    def __init__(self, array):
+        self.arr = array
+        self.modes = None
+        self.mode_index = 0 # permutacje: RGB, BGR, BRG ...
+        self.variant = 0  # 0 = interleaved, 1 = grouped
+        if self.arr.shape[1] == 3:
+            self.modes = list(permutations([0, 1, 2]))  # indeksy R=0, G=1, B=2
+        else:
+            self.modes = list(permutations([0, 1, 2, 3]))  # indeksy R=0, G=1, B=2, A=3
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.mode_index >= len(self.modes):
+            raise StopIteration
+
+        mode = self.modes[self.mode_index]
+
+        if self.variant == 0:
+            # Interleaved: R G B R G B ...
+            data = self.arr[:, list(mode)].flatten()
+        else:
+            # Grouped: RRR... GGG... BBB...
+            data = np.concatenate([self.arr[:, i] for i in mode])
+
+        self.variant += 1
+        if self.variant > 1:
+            self.variant = 0
+            self.mode_index += 1
+
+        return data
 
 class BitsIter4:
    def __init__(self,array):
@@ -147,30 +161,44 @@ class DecodingIter5:
       if self.i < 2:
          
          bytes_list = []
-         for i in range(self.j, len(self.arr), 8):
-            byte_bits = self.arr[i:i+8]
-            if len(byte_bits) < 8:
-                break
-            byte = np.packbits(byte_bits)[0]
-            bytes_list.append(byte)
-        
-         byte_data = bytes(bytes_list)
+
+         if self.j < self.mismatch:
+            bytes_list = self.arr[self.j: -(self.mismatch - self.j)]
+         else:
+            bytes_list = self.arr[self.mismatch:]
+
+         byte_data = np.packbits(bytes_list).tobytes()
+
          self.j += 1
 
          if self.j >= self.mismatch:
             self.i += 1
             self.j = 0
 
-         try:
-            if self.i == 0:
-                return byte_data.decode('utf-8')
-            else:
-               return byte_data.decode('ascii')
-         except UnicodeDecodeError:
-            return None
+         if self.i == 0:
+            return byte_data.decode('utf-8', errors='ignore')
+         else:
+            return byte_data.decode('ascii', errors='ignore')
       else:
          raise StopIteration
 
+
+def check(text,window_size,treshold,function):
+   if len(text) < window_size:
+      if function(window) > treshold:
+         return text
+
+   result = ""
+   for i in range(0,len(text),window_size):
+      window = text[i:i+window_size]
+      if function(window) > treshold:
+         result += window
+   return result
+
+def entropy_score(text: str) -> float:
+    letters = [c for c in text if c.isalpha()]
+    unique_letters = set(letters)
+    return len(unique_letters) / len(text)
 
 def character_distribution_score(text):
     english_freq = 'etaoinshrdlcumwfgypbvkjxqz'
@@ -205,6 +233,6 @@ def doubles_score(text):
 
 
 if __name__ == "__main__":
-    #run("../normal/YaK5lgBy0sunsplash.png",0.1)
-    run("../stego/LSB w BMP/zdj1sh.png",0.1)
+    #run("../normal/YaK5lgBy0sunsplash.png",50,0.2)
+    run("../stego/LSB w BMP/YaK5Long.png",20,0.2)
     pass
